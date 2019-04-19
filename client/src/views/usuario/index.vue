17400b7f89dbfcdb47c6b581157220f152348bb6
Edinéia Gonçalves <gedineia80@gmail.com>
2019-04-19 20:39:01 -0300

    Update index.vue

--- client/src/views/usuario/index.vue
+++ client/src/views/usuario/index.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
+    <div class="title is-pulled-left is-4">{{ title }}</div>
     <usuario-grid></usuario-grid>
   </div>
 </template>
 
 <script>
-import UsuarioGrid from './grid';
+import UsuarioGrid from "./grid";
 
 export default {
-  name: 'Usuario',
+  name: "Usuario",
   components: {
-    UsuarioGrid,
+    UsuarioGrid
+  },
+  data() {
+    return {
+      title: "Cadastro de usuários"
+    };
   }
-}
+};
 </script>
